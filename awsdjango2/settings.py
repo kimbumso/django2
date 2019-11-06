b02eb02ee10422b886c6a6e81a3b36754324c7fe
kimbumso <k101413@naver.com>
2019-11-06 16:34:46 +0900

    main page 수정

--- awsdjango2/settings.py
+++ awsdjango2/settings.py
@@ -81,8 +81,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'awsdjangodb',
-        'USER': '----',
-        'PASSWORD': '----',
+        'USER': 'test',
+        'PASSWORD': 'sexymild',
         'PORT': '3306',
         'HOST': 'localhost',
         'OPTIONS': {
